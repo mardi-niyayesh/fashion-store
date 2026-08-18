044c4a2a4b36e959d39de60e20230a546b005946
niyayesh <niyayeshammardii@gmail.com>
2026-08-18 11:42:22 -0700

    creat pages Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
 import Header from "@/Components/layout/Heder";
 import Footer from "@/Components/layout/Footer";
+import Banner from "@/feacher/Home/Banner";
+import Services from "@/feacher/Home/Services";
+import DescriptionStor from "@/feacher/Home/descriptionStor";
+import Container from "@/Components/Container";
+import FashionMagazine from "@/feacher/Home/FashionMagazine";
 export default function Home() {
   return (
     <>
       <Header />
+      <Banner />
+      <Container>
+        <DescriptionStor />
+      </Container>
+      <Services />
+      <Container>
+        <FashionMagazine />
+      </Container>
+
       <Footer />
     </>
   );
