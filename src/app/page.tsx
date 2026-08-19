1e096889250ff8b17b4ed8f19c1a0e9633a759d5
niyayesh <niyayeshammardii@gmail.com>
2026-08-19 05:46:31 -0700

    deleat component heder and footer to page and change location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,38 @@
-import Header from "@/Components/layout/Heder";
-import Footer from "@/Components/layout/Footer";
 import Banner from "@/feacher/Home/Banner";
 import Services from "@/feacher/Home/Services";
 import DescriptionStor from "@/feacher/Home/descriptionStor";
 import Container from "@/Components/Container";
 import FashionMagazine from "@/feacher/Home/FashionMagazine";
+import { bagProducts, womenProducts } from "@/feacher/Home/Product/DataProduct";
+import CategorySection from "@/feacher/Home/Product/CategorySection";
+import Question from "@/feacher/Home/question/Question";
+
 export default function Home() {
   return (
     <>
-      <Header />
       <Banner />
+
       <Container>
         <DescriptionStor />
       </Container>
+
       <Services />
+
       <Container>
         <FashionMagazine />
       </Container>
 
-      <Footer />
+      <CategorySection
+        title="لباس زنانه"
+        products={womenProducts}
+      />
+
+      <CategorySection
+        title="کیف"
+        products={bagProducts}
+      />
+
+      <Question />
     </>
   );
 }
