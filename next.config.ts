499165466f316e023f1b7292536fa8006bef3c20
niyayesh <niyayeshammardii@gmail.com>
2026-08-19 10:19:26 -0700

    fix bug img

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-
   basePath: "/fashion-store",
-
+  assetPrefix: "/fashion-store/",
   images: {
     unoptimized: true,
   },
--- src/feacher/Home/Product/DataProduct/index.tsx
+++ src/feacher/Home/Product/DataProduct/index.tsx
@@ -1,28 +1,29 @@
+const basePath = "/fashion-store";
 export const bagProducts = [
   {
     id: 5,
-    img: "/imgs/b1.jpg",
+    img: `${basePath}/imgs/b1.jpg`,
     title: "کیف دستی",
     description: "کیف شیک و مینیمال مناسب استفاده روزمره",
     price: 1200000,
   },
   {
     id: 6,
-    img: "/imgs/b2.jpg",
+    img: `${basePath}/imgs/b2.jpg`,
     title: "کیف دوشی",
     description: "طراحی کاربردی و مناسب استایل روزمره",
     price: 890000,
   },
   {
     id: 7,
-    img: "/imgs/b3.jpg",
+    img: `${basePath}/imgs/b3.jpg`,
     title: "کیف کوله پشتی",
     description: "انتخابی خاص برای استایل‌های رسمی",
     price: 1450000,
   },
   {
     id: 8,
-    img: "/imgs/b4.jpg",
+    img: `${basePath}/imgs/b4.jpg`,
     title: "کیف کوچک",
     description: "ظریف، سبک و مناسب استفاده روزانه",
     price: 750000,
@@ -32,28 +33,28 @@ export const bagProducts = [
 export const womenProducts = [
   {
     id: 1,
-    img: "/imgs/w1.jpg",
+    img: `${basePath}/imgs/w1.jpg`,
     title: "پیراهن زنانه",
     description: "پیراهن شیک و راحت برای استفاده روزمره",
     price: 1250000,
   },
   {
     id: 2,
-    img: "/imgs/w2.jpg",
+    img: `${basePath}/imgs/w2.jpg`,
     title: "کراپ زنانه",
     description: "طراحی مدرن با کیفیت و دوخت عالی",
     price: 1850000,
   },
   {
     id: 3,
-    img: "/imgs/w3.jpg",
+    img: `${basePath}/imgs/w3.jpg`,
     title: "شومیز زنانه",
     description: "انتخابی مناسب برای استایل‌های روزمره",
     price: 980000,
   },
   {
     id: 4,
-    img: "/imgs/w4.jpg",
+    img: `${basePath}/imgs/w4.jpg`,
     title: "تیشرت زنانه",
     description: "راحت و مناسب برای استایل روزانه",
     price: 1150000,
