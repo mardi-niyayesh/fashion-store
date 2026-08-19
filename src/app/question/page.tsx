215116bda2253fd9098897ec155010db546ad6f5
niyayesh <niyayeshammardii@gmail.com>
2026-08-19 05:45:25 -0700

    rout quesion

---
+++ src/app/question/page.tsx
@@ -0,0 +1,11 @@
+import Question from "@/feacher/Home/question/Question";
+
+const QuestionPage = () => {
+  return (
+    <div>
+      <Question />
+    </div>
+  );
+};
+
+export default QuestionPage;
